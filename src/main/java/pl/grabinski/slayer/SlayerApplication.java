f4df6594941112377e59a28ec88f2bcaa9e1be3a
maciek <whataterriblefailure@gmail.com>
2017-10-06 21:23:07 +0200

    basic Spring Boot application maven project (created using start.spring.io)

---
+++ src/main/java/pl/grabinski/slayer/SlayerApplication.java
@@ -0,0 +1,12 @@
+package pl.grabinski.slayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlayerApplication.class, args);
+    }
+}
