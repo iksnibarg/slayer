80cfa0c8468a83011ff36745158c75dfc3140596
maciek <whataterriblefailure@gmail.com>
2017-10-17 23:56:03 +0200

    improving tests for updating Instance list in repository

--- src/test/java/pl/grabinski/slayer/InstanceListUpdaterTest.java
+++ src/test/java/pl/grabinski/slayer/InstanceListUpdaterTest.java
@@ -9,16 +9,20 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InstanceListUpdaterTest {
 
     private static final String ID = "1";
+    private static final String OTHER_ID = "2";
     private static final String NAME = "RainbowDash";
     private static final String IMAGE_NAME = "Cirros";
     private static final String NOTES = "some very important notes";
@@ -33,6 +37,7 @@ public class InstanceListUpdaterTest {
     private Flavor flavor;
 
     private Instance someOpenStackOriginatedInstance = new Instance();
+    private Instance otherOpenStackOriginatedInstance = new Instance();
     private Instance repositoryInstance = new Instance();
 
     @Captor
@@ -50,10 +55,27 @@ public class InstanceListUpdaterTest {
         someOpenStackOriginatedInstance.setCreated(CREATED);
         someOpenStackOriginatedInstance.setStatus(STATUS);
 
+        otherOpenStackOriginatedInstance.setId(OTHER_ID);
+
         repositoryInstance.setId(ID);
         repositoryInstance.setNotes(NOTES);
 
         instanceListUpdater = new InstanceListUpdater(instanceRetriever, instanceRepository);
+    }
+
+    @Test
+    public void shouldUpdateExistingEntriesAndDeleteTheOthers() {
+        when(instanceRetriever.getInstances()).thenReturn(
+                Arrays.asList(someOpenStackOriginatedInstance, otherOpenStackOriginatedInstance));
+        when(instanceRepository.findOne(ID)).thenReturn(Optional.of(repositoryInstance));
+        when(instanceRepository.findOne(OTHER_ID)).thenReturn(Optional.empty());
+
+        instanceListUpdater.updateInstanceList();
+
+        verify(instanceRepository).deleteByIdNotIn(Arrays.asList(ID, OTHER_ID));
+        verify(instanceRepository, times(2)).save(savedInstanceCaptor.capture());
+        assertEquals(Arrays.asList(ID, OTHER_ID),
+                savedInstanceCaptor.getAllValues().stream().map(Instance::getId).collect(Collectors.toList()));
     }
 
     @Test
@@ -85,6 +107,7 @@ public class InstanceListUpdaterTest {
 
         verify(instanceRepository).save(savedInstanceCaptor.capture());
         Instance savedInstance = savedInstanceCaptor.getValue();
+        assertSame(repositoryInstance, savedInstance);
         verifyOpenStackOriginatedAttributesUpdated(savedInstance);
         assertEquals(NOTES, savedInstance.getNotes());
     }
